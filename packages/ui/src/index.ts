4fd38dd40e8db3c31ebd5331841799e871c15a8a
jaideepghosh <er.jaideepghosh@gmail.com>
2026-05-28 17:36:12 +0530

    feat: add dropdown menu component and update exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,6 +4,7 @@ export * from "./hooks/use-toast";
 export * from "./components/ui/badge";
 export * from "./components/ui/button";
 export * from "./components/ui/dialog";
+export * from "./components/ui/dropdown-menu";
 export * from "./components/ui/input";
 export * from "./components/ui/label";
 export * from "./components/ui/resizable";
